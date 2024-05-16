fa99474c933ab9d4faf1028570b0b0eb5857f7d8
Isidora <isidora.oyanedel@usach.cl>
2024-05-15 23:27:26 -0400

    yo

---
+++ autofix-car/src/main/java/com/example/autofixcar/AutofixCarApplication.java
@@ -0,0 +1,13 @@
+package com.example.autofixcar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutofixCarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutofixCarApplication.class, args);
+    }
+
+}
---
+++ autofix-car/src/test/java/com/example/autofixcar/AutofixCarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.autofixcar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutofixCarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
