0c32f42de550c14ac3b63464135a20f7e511ce2a
Isidora <isidora.oyanedel@usach.cl>
2024-05-24 21:22:37 -0400

    intentandoque funcione con gateway

--- autofix-car/src/main/java/com/example/autofixcar/controller/CarController.java
+++ autofix-car/src/main/java/com/example/autofixcar/controller/CarController.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.regex.Pattern;
 
 @RestController
-@RequestMapping("/car")
+@RequestMapping("/api/car")
 @CrossOrigin("*")
 public class CarController {
     @Autowired
